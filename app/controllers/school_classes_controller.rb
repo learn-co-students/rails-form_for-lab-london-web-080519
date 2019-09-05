532c41e02870a755cbcc77ebd4b853f8afb33493
99chris99 <chrishodge87@gmail.com>
2019-09-05 13:24:55 +0100

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,47 @@
+class SchoolsController < ApplicationController
+end
+class SchoolClassesController < ApplicationController
+
+def index
+    @school_classes = SchoolClass.all
+end
+
+def new
+    @class = SchoolClass.new
+end
+
+def create
+    school_class = SchoolClass.create(school_class_params)
+    redirect_to school_class
+end
+
+def show
+    @school_class = SchoolClass.find params[:id]
+end
+
+def edit
+    @school_class = SchoolClass.find params[:id]
+end
+
+def update
+    @school_class = SchoolClass.find params[:id]
+    @school_class.update(school_class_params)  
+    redirect_to school_class_path
+end
+
+# def update
+#     @article = Article.find(params[:id])
+#     @article.update(title: params[:article][:title], description: params[:article][:description])
+#     redirect_to article_path(@article)
+#   end
+
+end#class end
+
+
+private
+
+
+
+def school_class_params 
+    params.require(:school_class).permit(:title, :room_number)
+end
